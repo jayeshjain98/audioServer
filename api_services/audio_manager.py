6126bf1a9302208cde676369b3d00b003ceab7c3
Jayesh Jain <jayeshj@rawattech.com>
2021-05-08 21:12:16 +0530

    updated api services to handle database tasks

--- api_services/audio_manager.py
+++ api_services/audio_manager.py
@@ -1,6 +1,8 @@
 import json
 import falcon
 from falcon.media.validators import jsonschema
+from database_services.operation.dbUtil import dbUtil
+from api_services.json_parser import metadata_parser
 from utilities.load_schema import validate_audio_input
 
 
@@ -9,38 +11,50 @@ class save_audio(object):
 
     @jsonschema.validate(validate_audio_input) 
     def on_post(self, req, resp):
-        data = req.media
-        resp.body,resp.status = json.dumps({"Action is successful" : "200 OK"}), falcon.HTTP_200
+            data = req.media
+            filetype,parameters = metadata_parser(data)
+            dbUtil.doOperation(dbUtil.insert,parameters,filetype)
+            resp.text,resp.status = json.dumps({"Action is successful" : "200 OK"}), falcon.HTTP_200
+
 
 class delete_audio(object):
     """The class handles delete request to delete 3 different types of audio files from database"""
 
-
     def on_delete(self, req, resp, audioFileID, audioFileType):
+        audioFileID = str(audioFileID).upper() if audioFileType != None else None
         if audioFileType in ["Song", "Podcast", "Audiobook"]:
-            resp.body,resp.status = json.dumps({"Action is successful" : "200 OK"}), falcon.HTTP_200
+            parameters = (audioFileID,)
+            dbUtil.doOperation(dbUtil.delete,parameters,audioFileType)
+            resp.text,resp.status = json.dumps({"Action is successful" : "200 OK"}), falcon.HTTP_200
         else:
-            resp.body,resp.status = json.dumps({"The request is invalid": "400 bad request"}), falcon.HTTP_400
+            resp.text,resp.status = json.dumps({"The request is invalid": "400 bad request"}), falcon.HTTP_400
 
 class update_audio(object):
     """The class handles update request to update 3 different types of audio files in database"""
 
-    @jsonschema.validate(validate_audio_input) 
+    # @jsonschema.validate(validate_audio_input) 
     def on_put(self, req, resp, audioFileType, audioFileID):
+        # resp.text,resp.status = json.dumps({"Action is successful" : "200 OK"}), falcon.HTTP_200
         if audioFileType in ["Song", "Podcast", "Audiobook"]:
             data = req.media
-            print(data)
-            resp.body,resp.status = json.dumps({"Action is successful" : "200 OK"}), falcon.HTTP_200
+            filetype,parameters = metadata_parser(data)
+            if audioFileType == filetype:
+                dbUtil.doOperation(dbUtil.update,parameters[1:]+(str(audioFileID).upper(),),filetype)
+                resp.text,resp.status = json.dumps({"Action is successful" : "200 OK"}), falcon.HTTP_200
+            else:
+                resp.text,resp.status = json.dumps({"The request is invalid": "400 bad request"}), falcon.HTTP_400
         else:
-            resp.body,resp.status = json.dumps({"The request is invalid": "400 bad request"}), falcon.HTTP_400
+            resp.text,resp.status = json.dumps({"The request is invalid": "400 bad request"}), falcon.HTTP_400
+
 
 class get_audio(object):
     """The class handles get request to fetch 3 different types of audio files from database"""
 
-
     def on_get(self, req, resp, audioFileType, audioFileID = None):
+        audioFileID = str(audioFileID).upper() if audioFileID != None else None
         if audioFileType in ["Song", "Podcast", "Audiobook"]:
-            resp.body,resp.status = json.dumps({"Action is successful" : "200 OK"}), falcon.HTTP_200
+            parameters = (audioFileID,)
+            result = dbUtil.doOperation(dbUtil.get,parameters,audioFileType)
+            resp.text,resp.status = json.dumps({"filenames" : result}), falcon.HTTP_200
         else:
-            resp.body,resp.status = json.dumps({"The request is invalid": "400 bad request"}), falcon.HTTP_400
-            
+            resp.text,resp.status = json.dumps({"The request is invalid": "400 bad request"}), falcon.HTTP_400
