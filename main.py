d5fc5abbe80f468c0389616ba3ab8f0b189d5e7d
Jayesh Jain <jayeshj@rawattech.com>
2021-05-09 09:25:17 +0530

    renewed and removed comments

--- main.py
+++ main.py
@@ -1,45 +1,9 @@
-
-# {
-#   "audioFileType": "Song",
-#   "audioMetadata": {
-#     "song_upload_time": "2018-01-18T00:00:00",
-#     "song_id": "2EB8AA08-AA98-11EA-B4AA-73B441D16380",
-#     "song_name": "abcd",
-#     "song_duration": 1
-#   }
-# }
-
-# {
-#   "audioFileType": "Podcast",
-#   "audioMetadata": {
-#     "podcast_upload_time": "2018-01-18T00:00:00",
-#     "podcast_id": "2EB8AA08-AA98-11EA-B4AA-73B441D16380",
-#     "podcast_name": "abcd",
-#     "podcast_duration": 1,
-#     "host": "abc",
-#     "participants": [
-#       "jack"
-#     ]
-#   }
-# }
-
-# {
-#   "audioFileType": "Audiobook",
-#   "audioMetadata": {
-#     "audiobook_upload_time": "2018-01-18T00:00:00",
-#     "audiobook_id": "2EB8AA08-AA98-11EA-B4AA-73B441D16380",
-#     "audiobook_title": "abcd",
-#     "audiobook_duration": 1,
-#     "author": "abc",
-#     "narrator": "jack"
-#   }
-# }
 import falcon
 import json
 from api_services.audio_manager import save_audio, delete_audio, get_audio, update_audio
 from api_services.custom_error_handler import error_handler
 
-app = falcon.API()
+app = falcon.App()
 
 write_handler = save_audio()
 delete_handler = delete_audio()
